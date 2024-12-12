5d418f6f19687d651c415f52349d137fd5085ebc
DG-M-999 <diedatasel@gmail.com>
2024-12-12 21:42:21 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+from streamlit_option_menu import option_menu
+from numerize.numerize import numerize
+from query import *
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config(page_title="Dashboard",page_icon="🟡",layout="wide")
+st.subheader("🔔 Insurance Descriptive Analytics")
+st.markdown("##")
+
+result = view_all_data()
+df=pd.DataFrame(result,columns=["Policy","Expiry","Location","State","Region","Investment","Construction","BusinessType","Earthquake","Flood","Rating","id"])
+st.dataframe(df)
